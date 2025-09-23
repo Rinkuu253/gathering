2d3bc16c07d1499f17b7df5eee1f0903f79e4b60
MatthewM <cemhyp123@gmail.com>
2025-09-23 21:49:26 +0700

    new group page

--- src/page/dateRules.js
+++ src/page/dateRules.js
@@ -1,7 +1,7 @@
 export const dateRules = [
   {
     start: new Date("2025-09-21T00:00:00+07:00").getTime(),
-    end: new Date("2025-09-25T16:54:00+07:00").getTime(),
+    end: new Date("2025-09-23T16:54:00+07:00").getTime(),
     redirectTo: "/main/:kelompok",
   },
   {
--- src/page/group.jsx
+++ src/page/group.jsx
@@ -1,32 +1,81 @@
-import { Card, Flex, Group, Image, Text } from "@mantine/core";
+import { useEffect, useRef, useState } from "react";
+import {
+  Button,
+  Card,
+  Container,
+  Flex,
+  Image,
+  Text,
+  Title,
+} from "@mantine/core";
+import "../App.css";
 import { useParams } from "react-router-dom";
 import { listGroup } from "./dateRules";
 
-export default function GroupPage() {
+export default function MainPage() {
   const { kelompok } = useParams();
-
   return (
-    <Flex w="100%" h="100%" style={{ border: "2px solid black" }}>
-      <Card withBorder w="100%">
-        <Flex
-          h={"100%"}
-          align={"center"}
-          direction={"column"}
-          justify={"space-between"}
-          my={"xl"}
-          style={{ border: "2px solid blue" }}
-        >
-          <Flex justify="center" direction={"column"} gap={"md"}>
-            <Text fz={"xl"} fw={700}>
-              Selamat Datang Kelompok
-            </Text>
-            <Text fz={"lg"} fw={600}>
-              {listGroup?.[kelompok]?.name}
-            </Text>
-            <Image src={"/Group/CRYO.png"} w={"50%"} mx={"auto"} />
+    <>
+      <Container size={"xl"} px={"md"} m={0}>
+        <Card w="100%" bg={"transparent"} h={"85vh"}>
+          <Flex
+            h={"100%"}
+            align={"center"}
+            direction={"column"}
+            justify={"space-between"}
+            my={"xl"}
+          >
+            <Flex justify=" center" direction={"column"} gap={"xl"}>
+              <Text fz={"xl"} fw={700} c={"white"}>
+                Selamat Bermain Kelompok
+              </Text>
+              <Image
+                src={`/Group/${listGroup?.[kelompok]?.name}.png`}
+                w={"50%"}
+                mx={"auto"}
+              />
+            </Flex>
+            <Flex justify={"center"} direction={"column"} gap={"xl"} w={"100%"}>
+              <Button
+                fullWidth
+                size="lg"
+                radius={"lg"}
+                variant="outline"
+                c={"white"}
+              >
+                Hint
+              </Button>
+              <Button
+                fullWidth
+                size="lg"
+                radius={"lg"}
+                variant="outline"
+                c={"white"}
+              >
+                List Peta
+              </Button>
+              <Button
+                fullWidth
+                size="lg"
+                radius={"lg"}
+                variant="outline"
+                c={"white"}
+              >
+                List Kamar
+              </Button>
+              <Button
+                fullWidth
+                size="lg"
+                radius={"lg"}
+                variant="outline"
+                c={"white"}
+              >
+                List Kelompok
+              </Button>
+            </Flex>
           </Flex>
-        </Flex>
-      </Card>
-    </Flex>
+        </Card>
+      </Container>
+    </>
   );
 }
