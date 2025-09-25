cd6447e5f172b113dfae219ddb83a3cad64e8dee
MatthewM <cemhyp123@gmail.com>
2025-09-25 20:03:01 +0700

    new main page for night and tomorrow

--- src/page/mainPage.jsx
+++ src/page/mainPage.jsx
@@ -34,7 +34,12 @@ export default function MainPage() {
   );
   const inRange3 = isBetween(
     "2025-09-25T14:00:00+07:00", // mulai 24 Sept 2025 jam 00:00 WIB
-    "2025-09-26T08:00:00+07:00" // sampai 26 Sept 2025 jam 08:00 WIB
+    "2025-09-25T19:00:00+07:00" // sampai 26 Sept 2025 jam 08:00 WIB
+  );
+
+  const inRange4 = isBetween(
+    "2025-09-25T20:00:00+07:00", // mulai 24 Sept 2025 jam 00:00 WIB
+    "2025-09-26T04:00:00+07:00" // sampai 26 Sept 2025 jam 08:00 WIB
   );
 
   return inRange ? (
@@ -43,6 +48,8 @@ export default function MainPage() {
     <MainPageSecondDay />
   ) : inRange3 ? (
     <MainPageSecondDayHalf />
+  ) : inRange4 ? (
+    <MainPageSecondDayNight />
   ) : (
     <MainPageThirdDay />
   );
@@ -362,6 +369,101 @@ const MainPageSecondDayHalf = () => {
   );
 };
 
+const MainPageSecondDayNight = () => {
+  const audioRef = useRef(null);
+
+  const [showIcon, setShowIcon] = useState(false);
+
+  const handleTextClick = () => {
+    const audio = audioRef.current;
+    if (audio) {
+      audio.muted = false;
+      audio.play();
+      setShowIcon(true); // show the volume icon
+    }
+  };
+
+  const handleIconClick = () => {
+    const audio = audioRef.current;
+    if (!audio) return;
+
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  };
+  return (
+    <>
+      <Container size={"xl"} px={"md"} m={0}>
+        <Flex
+          direction={"column"}
+          h={"100vh"}
+          w={"100%"}
+          py={"xl"}
+          px={"xl"}
+          align={"center"}
+          mx={"auto"}
+          gap={"md"}
+        >
+          <Text fz={"h3"} fw={700}>
+            Beyond Journey - <br />
+            The Rise of Agents
+          </Text>
+          <Group>
+            {!showIcon && (
+              <Text
+                fz={"xs"}
+                style={{ cursor: "pointer", userSelect: "none" }}
+                onClick={handleTextClick}
+              >
+                {`> Tap for Audio <`}
+              </Text>
+            )}
+
+            {showIcon && (
+              <IconVolume
+                color="white"
+                size={24}
+                style={{ cursor: "pointer" }}
+                onClick={handleIconClick}
+              />
+            )}
+          </Group>
+          <ScrollArea mah={"90vh"}>
+            <Flex
+              gap={"lg"}
+              direction={"column"}
+              mt={"md"}
+              maw={800}
+              mx={"auto"}
+            >
+              <Text fz={"xs"}>Bayangan itu tidak sendirian…</Text>
+              <Text fz={"xs"}>
+                Mereka berulang, menyusup, dan menempel di setiap celah
+                pekerjaanmu. Jumlahnya tak kecil, tapi juga bukan puluhan.
+              </Text>
+
+              <Text fz={"xs"}>
+                Coba lihat bentuk yang sempurna… delapan sisi, delapan arah. Ya…
+                begitulah banyaknya mereka yang harus kalian hadapi.
+              </Text>
+              <Text fz={"md"} fw={900}>
+                Hati-hati, karena tiap musuh punya wajah berbeda, namun tujuan
+                mereka sama: memperlambatmu.
+              </Text>
+            </Flex>
+          </ScrollArea>
+
+          <audio ref={audioRef} autoPlay loop muted>
+            <source src="/music/halfSecondDay.mpeg" type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        </Flex>
+      </Container>
+    </>
+  );
+};
 const MainPageThirdDay = () => {
   const audioRef = useRef(null);
 
@@ -422,41 +524,53 @@ const MainPageThirdDay = () => {
               />
             )}
           </Group>
-          <Flex gap={"lg"} direction={"column"} mt={"xl"} maw={800} mx={"auto"}>
-            <Text fz={"xs"}>Perjalanan ini bukan perjalanan biasa.</Text>
-            <Text fz={"xs"}>
-              Perjalanan ini bukan perjalanan biasa. Dalam hitungan menit,
-              identitas barumu akan diaktifkan.
-            </Text>
-            <Text fz={"xs"}>
-              Anda bukan lagi hanya seorang peserta — anda akan resmi menjadi{" "}
-              <Text span fw={900} fz={"xs"}>
-                Agent
+          <ScrollArea mah={"70vh"}>
+            <Flex
+              gap={"lg"}
+              direction={"column"}
+              mt={"xl"}
+              maw={800}
+              mx={"auto"}
+            >
+              <Title fz={"xl"}>Welcome, agen terpilih.</Title>
+              <Text fz={"xs"}>
+                Perjalanan kalian baru saja dimulai… dan kereta ini bukan
+                sekadar alat transportasi, melainkan gerbang menuju misi yang
+                akan menguji ketahanan, kecerdasan, dan strategi kalian.
               </Text>
-              , dengan{" "}
-              <Text span fw={900} fz={"xs"}>
-                ID
-              </Text>{" "}
-              yang menandai siapa dirimu dalam misi ini. Tiket Woosh-mu bukan
-              sekadar akses perjalanan… ia adalah kunci menuju dunia penuh
-              rahasia, tantangan, dan Villain yang menunggu untuk diungkap.
-            </Text>
-            <Text fz={"xs"}>
-              Dengarkan baik-baik: setiap kode, setiap simbol, setiap langkah
-              akan menentukan apakah Squad-mu akan bertahan… atau tersingkir
-              dalam bayangan sejarah.
-            </Text>
+              <Text fz={"xs"}>
+                Di depan, bukan sekadar destinasi. Ada bayangan yang menunggu.
+                Mereka tidak berwujud manusia, tetapi selalu hadir di setiap
+                sistem, setiap proses, bahkan di sekitar kalian tanpa disadari.
+              </Text>
+              <Text fz={"xs"}>
+                Jumlah mereka… sama dengan sisi dari bentuk sempurna. Delapan
+                wajah, delapan musuh, delapan bayangan. Masing-masing memiliki
+                cara unik untuk mengacaukan ritme dan menunda kemenangan.
+              </Text>
+              <Text fz={"xs"}>
+                Tugas kalian jelas: temukan mereka, kenali kelemahannya, dan
+                hancurkan satu per satu. Tapi waspadalah… mereka akan
+                bersembunyi di balik kelengahan, menyusup di setiap langkah, dan
+                menguji kekuatan tim kalian.
+              </Text>
 
-            <Text fz={"sm"} fw={700}>
-              Bersiaplah. Agent ID-mu dan Tiket Woosh sebentar lagi tiba
-              <br />
-            </Text>
-            <Text fw={900} fz={"lg"} mt={"md"}>
-              Siap berangkat, Agent?
-            </Text>
-          </Flex>
+              <Text fz={"xs"}>
+                Nikmati perjalanan ini.
+                <br />
+                Gunakan waktu di Whoosh ini untuk menenangkan pikiran,
+                menyiapkan strategi, dan menyatukan semangat. Karena begitu
+                pintu misi terbuka… tidak ada jalan kembali. Kemenangan hanya
+                untuk agen yang sigap, solid, dan berani.
+              </Text>
+              <Text fw={900} fz={"lg"} mt={"md"}>
+                Segera temukan Agent ID kamu untuk mengenali identitas musuh !
+              </Text>
+            </Flex>
+          </ScrollArea>
+
           <audio ref={audioRef} autoPlay loop muted hidden>
-            <source src="/music/sherma.mp3" type="audio/mpeg" />
+            <source src="/music/whoosh.mp3" type="audio/mpeg" />
             Your browser does not support the audio element.
           </audio>
         </Flex>
